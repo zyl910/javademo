85990ab9c0ee31436cb574c7f0e3326707788403
zyl910 <zyl910@sina.com>
2019-05-07 15:22:56 +0800

    charinvalidxerces: 分析转义字符报异常的规律.

--- xml/charinvalidxerces/src/org/zyl910/javademo/xml/charinvalidxerces/Main.java
+++ xml/charinvalidxerces/src/org/zyl910/javademo/xml/charinvalidxerces/Main.java
@@ -2,6 +2,7 @@ package org.zyl910.javademo.xml.charinvalidxerces;
 
 import org.apache.axis.encoding.DeserializationContext;
 import org.apache.axis.utils.XMLUtils;
+import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.ext.LexicalHandler;
@@ -21,6 +22,48 @@ public class Main extends DefaultHandler implements LexicalHandler {
 
     public void setOuts(PrintStream outs) {
         this.outs = outs;
+    }
+
+    /** 处理.
+     */
+    private void process() throws Exception {
+        //String strxml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><root>  <str>汉字ABC&amp;#16;.</str></root>";  // 不会解转义.
+            //startElement: str
+            //characters: 汉字ABC
+            //startEntity: amp
+            //characters: &
+            //endEntity: amp
+            //characters: #16
+            //characters: ;.
+            //endElement: str
+        //String strxml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><root>  <str>汉字ABC&amp;#16;&#16;.</str></root>"; // 异常. org.xml.sax.SAXParseException; lineNumber: 1; columnNumber: 71; Character reference "&#16" is an invalid XML character.
+            //startElement: str
+            //characters: 汉字ABC
+            //startEntity: amp
+            //characters: &
+            //endEntity: amp
+            //characters: #16
+            //characters: ;
+            //org.xml.sax.SAXParseException; lineNumber: 1; columnNumber: 71; Character reference "&#16" is an invalid XML character.
+        //String strxml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><root>  <str><![CDATA[汉字ABC&amp;#16;&#16;.]]></str></root>"; // 不会解转义. 会被提取为“C&amp;#16;&#16;”.
+            //startElement: str
+            //startCDATA
+            //characters: 汉字AB
+            //characters: C&amp;#16;&#16;.
+            //endCDATA
+            //endElement: str
+        //String strxml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><root>  <str>A&#160;B</str></root>";  // 正常转义.
+            //characters: A
+            //characters:  
+            //characters: B
+        String strxml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><root>  <str>A&#31;B</str></root>";  // 异常. 虽然xml支持转义字符, 但若该字符属于xml非法字符范围内, 则会报异常.
+            //startElement: str
+            //characters: A
+            //org.xml.sax.SAXParseException; lineNumber: 1; columnNumber: 58; Character reference "&#31" is an invalid XML character.
+        InputSource is;
+        is = new InputSource(new StringReader(strxml));
+        //testAxis(is);
+        testJaxp(is);
     }
 
     /** 测试axis解析回应xml数据.
@@ -47,16 +90,6 @@ public class Main extends DefaultHandler implements LexicalHandler {
         outs.println(parser);
     }
 
-    /** 处理.
-     */
-    private void process() throws Exception {
-        String strxml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><root>  <str>汉字ABC&amp;#16;.</str></root>";
-        InputSource is;
-        is = new InputSource(new StringReader(strxml));
-        //testAxis(is);
-        testJaxp(is);
-    }
-
     @Override
     public void startDTD(String name, String publicId, String systemId) throws SAXException {
         outs.println("startDTD: " + name);
@@ -64,17 +97,17 @@ public class Main extends DefaultHandler implements LexicalHandler {
 
     @Override
     public void endDTD() throws SAXException {
-        outs.println("startCDATA");
+        outs.println("endDTD");
     }
 
     @Override
     public void startEntity(String name) throws SAXException {
-        outs.println("startDTD: " + name);
+        outs.println("startEntity: " + name);
     }
 
     @Override
     public void endEntity(String name) throws SAXException {
-        outs.println("startDTD: " + name);
+        outs.println("endEntity: " + name);
     }
 
     @Override
@@ -84,13 +117,32 @@ public class Main extends DefaultHandler implements LexicalHandler {
 
     @Override
     public void endCDATA() throws SAXException {
-        outs.println("startCDATA");
+        outs.println("endCDATA");
     }
 
     @Override
     public void comment(char[] ch, int start, int length) throws SAXException {
         String str = new String(ch, start, length);
-        outs.println("startDTD: " + str);
+        outs.println("comment: " + str);
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        outs.println("startElement: " + qName);
+        super.startElement(uri, localName, qName, attributes);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        outs.println("endElement: " + qName);
+        super.endElement(uri, localName, qName);
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        String str = new String(ch, start, length);
+        outs.println(String.format("characters(%d): %s", length, str));
+        super.characters(ch, start, length);
     }
 
     public static void main(String[] args) {
