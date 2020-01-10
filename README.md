494932be884b301e84c81e8f5c64f6940e51aca2
zyl910 <zyl910@sina.com>
2020-01-10 16:48:52 +0800

    studyjniMaven: Study Jni on Maven

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # javademo
 Java demo
+
+- `jdk/jdk8demo`: Java 8 demo.
+- `jdk/studyjni`: Study Jni.
+- `jdk/studyjniMaven`: Study Jni on Maven.
+- `xml/charinvalidxerces`: 研究 jaxp 解析xml时的非法字符问题.
+
---
+++ jdk/studyjniMaven/src/main/java/org/e3/studyjni/Main.java
@@ -0,0 +1,11 @@
+package org.e3.studyjni;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello studyjniMaven(Study Jni on Maven)!");
+        System.out.println();
+        System.out.println("System.getProperties():");
+        System.getProperties().list(System.out);
+    }
+}
