3beab038b5576a62dba1731f9fc1fef146b457d9
zyl910 <zyl910@sina.com>
2020-01-10 17:20:04 +0800

    studyjni: TestCPrintf add C language function (toupper/pow).

--- jdk/studyjni/src/main/java/org/e3/studyjni/TestCPrintf.java
+++ jdk/studyjni/src/main/java/org/e3/studyjni/TestCPrintf.java
@@ -11,17 +11,23 @@ public class TestCPrintf {
     // customization and mapping of Java to native types.
 
     public interface CLibrary extends Library {
-        CLibrary INSTANCE = (CLibrary)
-                Native.load((Platform.isWindows() ? "msvcrt" : "c"),
-                        CLibrary.class);
-
+        // 它们是C语言函数库中的函数，其定义格式如下:
+        // int toupper(int ch)
+        // double pow( double x, double y )
+        // int printf(const char* format, ...)
+        int toupper(int ch);
+        double pow(double x,double y);
         void printf(String format, Object... args);
     }
 
     public static void main(String[] args) {
-        CLibrary.INSTANCE.printf("Hello, World\n");
+        String libname = Platform.isWindows() ? "msvcrt" : "c";
+        CLibrary INSTANCE = (CLibrary)Native.load(libname, CLibrary.class);
+        INSTANCE.printf("Hello, World\n");
+        System.out.println("pow(2d,3d)=="+INSTANCE.pow(2d, 3d));
+        System.out.println("toupper('a')=="+(char)INSTANCE.toupper((int)'a'));
         for (int i=0;i < args.length;i++) {
-            CLibrary.INSTANCE.printf("Argument %d: %s\n", i, args[i]);
+            INSTANCE.printf("Argument %d: %s\n", i, args[i]);
         }
     }
 
